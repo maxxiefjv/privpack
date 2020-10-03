3294baa21c795d1b015b69ba86f2777419305df3
Max Vasterd <max@vasterd.nl>
2020-10-03 18:21:09 +0200

    added basic setup for testing the MultivariateGaussian statistcs: MI and Schur Complement

---
+++ tests/utils/test_statistics.py
@@ -0,0 +1,37 @@
+from privpack.utils import DataGenerator
+from privpack.utils.statistics import MultivariateGaussianMutualInformation
+
+import torch
+import numpy as np
+import pytest
+
+
+@pytest.fixture
+def train_data():
+    """
+    The generated data consists of 10 columns, generated using the DataGenerator.get_ppan_distribution_params function.
+    """
+    return np.loadtxt('static_train_data.csv', delimiter=',')
+
+
+@pytest.fixture
+def test_data():
+    """
+    The generated data consists of 10 columns, generated using the DataGenerator.get_ppan_distribution_params function.
+    """
+    return np.loadtxt('static_test_data.csv', delimiter=',')
+
+def test_compute_schur_complement(train_data, test_data):
+    multivariate_gauss_statistic = MultivariateGaussianMutualInformation('I(X;Y)')
+
+    train_cov = torch.Tensor(np.cov(train_data.T))
+    schur_complement = multivariate_gauss_statistic._compute_schur_complement(train_cov, 5)
+
+    assert schur_complement.size() == torch.Size([5, 5])
+    assert False
+
+
+def test_multivariate_gaussian_mi(train_data, test_data):
+    multivariate_gauss_statistic = MultivariateGaussianMutualInformation('I(X;Y)')
+    # mi = multivariate_gauss_statistic(train_x, train_y)
+    assert False
