980c88ffc26926e3cb147645cefca60af2deac10
Max Vasterd <max@vasterd.nl>
2020-10-06 14:07:55 +0200

    Added schur complement tests for fixed training data and random training data

--- tests/utils/test_statistics.py
+++ tests/utils/test_statistics.py
@@ -7,49 +7,43 @@ import pytest
 
 
 @pytest.fixture
-def train_data():
-    """
-    The generated data consists of 10 columns, generated using the DataGenerator.get_ppan_distribution_params function.
-    """
-    return np.loadtxt('tests/data/static_train_data.csv', delimiter=',')
-
-
-@pytest.fixture
-def test_data():
-    """
-    The generated data consists of 10 columns, generated using the DataGenerator.get_ppan_distribution_params function.
-    """
-    return np.loadtxt('tests/data/static_test_data.csv', delimiter=',')
-
-@pytest.fixture
 def get_random_covariance_matrix():
     def _create_random_covariance_matrix(n):
-        return np.power(np.random.uniform(0, 1, (n, n)), 2) * 10
+        return np.power(np.random.normal(size=(n, n)), 2) * 5
 
     return _create_random_covariance_matrix
 
-def test_compute_schur_complement_static_train_data(train_data):
+def test_compute_schur_complement_fixed_train_data(fixed_train_data):
     multivariate_gauss_statistic = MultivariateGaussianMutualInformation('I(X;Y)')
 
-    train_cov = torch.Tensor(np.cov(train_data.T))
+    train_cov = torch.Tensor(np.cov(fixed_train_data.T))
     schur_complement = multivariate_gauss_statistic._compute_schur_complement(train_cov, 5)
 
     assert schur_complement.size() == torch.Size([5, 5])
-    assert True
+    assert torch.det(schur_complement) > 0
 
-
-def test_multivariate_gaussian_mi(train_data, test_data):
+def test_compute_schur_complement__on_100_random_train_data_samples(get_random_train_data):
     multivariate_gauss_statistic = MultivariateGaussianMutualInformation('I(X;Y)')
-    # mi = multivariate_gauss_statistic(train_x, train_y)
-    assert True
 
-def test_compute_schur_complement(train_data, get_random_covariance_matrix):
+    for i in range(100):
+        train_cov = torch.Tensor(np.cov(get_random_train_data().T))
+        schur_complement = multivariate_gauss_statistic._compute_schur_complement(train_cov, 5)
+
+        assert schur_complement.size() == torch.Size([5, 5])
+        assert torch.det(schur_complement) > 0
+
+
+def test_compute_schur_complement_on_10_random_cov_matrices(fixed_train_data, get_random_covariance_matrix):
     multivariate_gauss_statistic = MultivariateGaussianMutualInformation('I(X;Y)')
 
     for i in range(10):
         cov_matrix = torch.Tensor(get_random_covariance_matrix(10))
         schur_complement = multivariate_gauss_statistic._compute_schur_complement(cov_matrix, 5)
 
-        print(i, cov_matrix)
         assert schur_complement.size() == torch.Size([5, 5])
         assert torch.det(schur_complement) > 0
+
+# def test_multivariate_gaussian_mi(fixed_train_data, fixed_test_data):
+#     multivariate_gauss_statistic = MultivariateGaussianMutualInformation('I(X;Y)')
+#     # mi = multivariate_gauss_statistic(train_x, train_y)
+#     assert True
