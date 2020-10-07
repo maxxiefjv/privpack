65a744c018f47f692813e4e17dc5d5c7f77e5daf
Max Vasterd <max@vasterd.nl>
2020-10-07 15:32:03 +0200

    setup network validation structure

---
+++ privpack/model_selection/hyperparameter_tuning.py
@@ -0,0 +1,8 @@
+
+class HyperParameters():
+
+    def __init__(self):
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.hidden_layers_width = hidden_layers_width
+        self.release_size = release_size
