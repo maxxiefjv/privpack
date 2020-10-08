124f7c055a1bbfe4ce011564a1442bffcdd86711
Max Vasterd <max@vasterd.nl>
2020-10-08 13:27:25 +0200

    improved random matrix fixture to be a potential covariance matrix being PD.

--- tests/utils/test_metrics.py
+++ tests/utils/test_metrics.py
@@ -9,7 +9,9 @@ import pytest
 @pytest.fixture
 def get_random_covariance_matrix():
     def _create_random_covariance_matrix(n):
-        return np.power(np.random.normal(size=(n, n)), 2) * 5
+        return np.power(np.diag(np.random.normal(size=(n))), 2) * 5
+        # A = np.diag(np.random.normal(size=(n)))
+        # return A * A.T
 
     return _create_random_covariance_matrix
 
@@ -32,12 +34,10 @@ def test_compute_schur_complement__on_100_random_train_data_samples(get_random_t
         assert schur_complement.size() == torch.Size([5, 5])
         assert torch.det(schur_complement) > 0
 
-
-@pytest.mark.skip(reason="Test is currently failing. Implementation should be solved.")
 def test_compute_schur_complement_on_10_random_cov_matrices(fixed_train_data, get_random_covariance_matrix):
     multivariate_gauss_statistic = MultivariateGaussianMutualInformation('I(X;Y)')
 
-    for i in range(10):
+    for i in range(100):
         cov_matrix = torch.Tensor(get_random_covariance_matrix(10))
         schur_complement = multivariate_gauss_statistic._compute_schur_complement(cov_matrix, 5)
 
