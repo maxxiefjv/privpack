2b2517945849030c58a8029fcab18e57693aa14b
Max Vasterd <max@vasterd.nl>
2020-10-14 14:53:00 +0200

    removed unused file

--- privpack/model_selection/hyperparameter_tuning.py
+++
@@ -1,8 +0,0 @@
-
-class HyperParameters():
-
-    def __init__(self):
-        self.epochs = epochs
-        self.batch_size = batch_size
-        self.hidden_layers_width = hidden_layers_width
-        self.release_size = release_size
