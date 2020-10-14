897eaf8b64bb813e95ebd908b3a7b0c55e1cba07
Max Vasterd <max@vasterd.nl>
2020-10-14 15:38:04 +0200

    added flake8 recommended spacing

--- tests/model_selection/test_experiment.py
+++ tests/model_selection/test_experiment.py
@@ -24,8 +24,8 @@ def test_experiment(uncorrelated_train_and_test_data):
     network = BinaryGAN(torch.device('cpu'), network_criterion)
 
     expectations = Expectations()
-    expectations.add_expectation(PartialBivariateBinaryMutualInformation('I(X;Z)', 0), 0, lambda x,y: np.isclose(x,y))
-    expectations.add_expectation(PartialBivariateBinaryMutualInformation('I(Y;Z)', 1), 1, lambda x,y: np.isclose(x,y))
+    expectations.add_expectation(PartialBivariateBinaryMutualInformation('I(X;Z)', 0), 0, lambda x,y: np.isclose(x, y))
+    expectations.add_expectation(PartialBivariateBinaryMutualInformation('I(Y;Z)', 1), 1, lambda x,y: np.isclose(x, y))
     expectations.add_expectation(ComputeDistortion('E[hamm(x,y)]', 1).set_distortion_function(lambda x, y: hamming_distance(x, y).to(torch.float64)), 0.5, lambda x,y: x < y)
 
     experiment = Experiment(network, expectations)
