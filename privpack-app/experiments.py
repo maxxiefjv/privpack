47acb01dd81871a9974e4ab0db7b6bc2dea77c7b
Max Vasterd <max@vasterd.nl>
2020-11-23 11:22:01 +0100

    added selectable loss function to experiments class

--- privpack-app/experiments.py
+++ privpack-app/experiments.py
@@ -4,6 +4,8 @@ from privpack import GaussianPrivacyPreservingAdversarialNetwork as GaussGAN
 from privpack.core.criterion import PGANCriterion
 from privpack.core.criterion import BinaryHammingDistance, BinaryMutualInformation, NegativeBinaryMutualInformation
 from privpack.core.criterion import MeanSquaredError, GaussianMutualInformation, NegativeGaussianMutualInformation
+from privpack.core.criterion import BinaryAlphaLeakage, NegativeBinaryAlphaLeakage
+from privpack.core.criterion import BinaryMaximalLeakage, NegativeBinaryMaximalLeakage
 
 from privpack.model_selection.experiment import Experiment, Expectations
 from privpack.utils.metrics import PartialBivariateBinaryMutualInformation, PartialMultivariateGaussianMutualInformation, ComputeDistortion
@@ -17,35 +19,36 @@ class ExperimentRunner():
     def __init__(self):
         pass
 
-
 class BinaryExperiment(ExperimentRunner):
 
     def __init__(self):
-        pass
+        self.binary_criterion_switch = {
+            'mi': lambda lambd, delta, x: PGANCriterion().add_privacy_criterion(BinaryMutualInformation()).add_privacy_criterion(BinaryHammingDistance(lambd, delta)).add_adversary_criterion(NegativeBinaryMutualInformation()),
+            'maxl': lambda lambd, delta, x: PGANCriterion().add_privacy_criterion(BinaryMaximalLeakage()).add_privacy_criterion(BinaryHammingDistance(lambd, delta)).add_adversary_criterion(NegativeBinaryMaximalLeakage()),
+            'alpha': lambda lambd, delta, x: PGANCriterion().add_privacy_criterion(BinaryAlphaLeakage(x)).add_privacy_criterion(BinaryHammingDistance(lambd, delta)).add_adversary_criterion(NegativeBinaryAlphaLeakage(x)),
+        }
 
-    def run(self, data, epochs, batch_size, lambd, delta, verbose=False):
+    def run(self, data, epochs, batch_size, lambd, delta, criterion, alphas, verbose=False):
         results = {}
         n_splits = 3
 
         for d in delta:
             for l in lambd:
-                network_criterion = PGANCriterion()
-                network_criterion.add_privacy_criterion(BinaryHammingDistance(lambd=l, delta_constraint=d)).add_privacy_criterion(BinaryMutualInformation())
-                network_criterion.add_adversary_criterion(NegativeBinaryMutualInformation())
+                for a in alphas:
+                    network_criterion = self.binary_criterion_switch[criterion](l, d, a)
 
-                network = BinaryGAN(torch.device('cpu'), network_criterion)
+                    network = BinaryGAN(torch.device('cpu'), network_criterion)
 
-                expectations = Expectations()
-                expectations.add_expectation(PartialBivariateBinaryMutualInformation('I(X;Z)', 0), 0, lambda x,y: np.isclose(x, y))
-                expectations.add_expectation(PartialBivariateBinaryMutualInformation('I(Y;Z)', 1), 1, lambda x,y: np.isclose(x, y))
-                expectations.add_expectation(ComputeDistortion('E[hamm(x,y)]', [1]).set_distortion_function(lambda x, y: hamming_distance(x, y).to(torch.float64)), 0.5, lambda x,y: x < y)
+                    expectations = Expectations()
+                    expectations.add_expectation(PartialBivariateBinaryMutualInformation('I(X;Z)', 0), 0, lambda x,y: np.isclose(x, y))
+                    expectations.add_expectation(PartialBivariateBinaryMutualInformation('I(Y;Z)', 1), 1, lambda x,y: np.isclose(x, y))
+                    expectations.add_expectation(ComputeDistortion('E[hamm(x,y)]', [1]).set_distortion_function(lambda x, y: hamming_distance(x, y).to(torch.float64)), 0.5, lambda x,y: x < y)
 
-                experiment = Experiment(network, expectations)
-                runs_results = experiment.run(data, n_splits=n_splits, epochs=epochs, batch_size=batch_size, verbose=verbose)
-                results.setdefault(d, {}).setdefault(l, runs_results['averages'])
+                    experiment = Experiment(network, expectations)
+                    runs_results = experiment.run(data, n_splits=n_splits, epochs=epochs, batch_size=batch_size, verbose=verbose)
+                    results.setdefault(a, {}).setdefault(d, {}).setdefault(l, runs_results)
 
         return results
-
 
 class GaussianExperiment(ExperimentRunner):
 
